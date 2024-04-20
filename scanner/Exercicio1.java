e9226f94eca05ced3f1822f7d54595e84dedd40e
sabssssssss <166922347+sabssssssss@users.noreply.github.com>
2024-04-20 10:14:45 -0300

    Atualizar o Exercicio1.java

--- scanner/Exercicio1.java
+++ scanner/Exercicio1.java
@@ -2,6 +2,8 @@ public class Exercicio1 {
     public static void main(String[] args) {
         var sc = new Scanner(System.in);
         var nome = sc.nextLine ();
+       
+        System.out.println("boas vindas" + nome)
 
 
     }
